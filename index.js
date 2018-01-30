a5b52ddfd28414880622552c5ee5f149d3052c07
zy <zzcwzhang@sina.com>
2018-01-30 14:27:46 +0800

    add theme scan

--- index.js
+++ index.js
@@ -10,16 +10,75 @@ app.get('/all.do', (req, res) => {
 
 app.get('/scan/:from', (req, res) => {
     let from = req.params.from;
-    // from = encodeURI(from);
-    console.log(from);
-
     dbApi.getFrom(from).then(json=>{
         res.json(json)
     });
 });
 
-const server = app.listen(3000, function () {
-    const host = server.address().address;
+app.get('/theme/add/:name', (req, res) => {
+    let name = req.params.name;
+    dbApi.addTheme(name).then( json => {
+        res.json({
+            success: true
+        });
+    }).catch( err => {
+        res.json({
+            success: false,
+            info: err
+        });
+    })
+});
+
+app.get('/theme/add/:name/:keyword', (req, res) => {
+    let name = req.params.name;
+    let keyword = req.params.keyword;
+    dbApi.addKeyWord(name, keyword).then( json => {
+        res.json({
+            success: true,
+            info: json
+        });
+    }).catch( err => {
+        res.json({
+            success: false,
+            info: err
+        });
+    })
+});
+app.get('/theme/filter/:name/:keyword', (req, res) => {
+    let name = req.params.name;
+    let keyword = req.params.keyword;
+    dbApi.addFilterWord(name, keyword).then( json => {
+        res.json({
+            success: true,
+            info: json
+        });
+    }).catch( err => {
+        res.json({
+            success: false,
+            info: err
+        });
+    })
+});
+
+app.get('/theme/scan/:name', (req, res) => {
+    let name = req.params.name;
+    dbApi.getBytheme(name).then(json=>{
+        dbApi.getFilterByTheme(name).then(
+            //过滤数组
+            filters => {
+                //将过滤数组变成正则
+                const reg = `(${filters.join('|')})`;
+                const regx = new RegExp(reg);
+                json = json.filter( item =>!(regx.test(item.name)));
+                res.json(json)
+            }
+        ).catch(()=>res.json(json));
+    }).catch( error => {
+        res.json({})
+    });
+});
+const server = app.listen(3001, function () {
+    const host = server.address().address || 'localhost';
     const port = server.address().port;
     console.log('Express app listening at http://%s:%s', host, port);
 });
--- model/mongodata.js
+++ model/mongodata.js
@@ -11,11 +11,13 @@ const getAll = new Promise( (resolve, reject) => {
         const col = client.db(dbName).collection('ty_gp');
         col.find().toArray( (err, items) => {
             if (err) reject(err);
-            resolve(items);
             client.close();
+            resolve(items);
         });
     });
 });
+
+// getAll.then( res => console.log(res));
 
 const getFrom = from => new Promise( (resolve, reject) => {
     MongoClient.connect(url, (err, client) => {
@@ -29,9 +31,115 @@ const getFrom = from => new Promise( (resolve, reject) => {
     });
 });
 
+//添加主题
+const addTheme = theme => new Promise( (resolve, reject) => {
+    MongoClient.connect(url, (err, client) => {
+        if (err) throw err;
+        const col = client.db(dbName).collection('theme');
+        col.insert({'name': theme},(err, result) => {
+            if (err) reject(err);
+            resolve('insert success', result);
+            client.close();
+        });
+    });
+});
+
+// addTheme('主题测试');
+
+//给主题添加关键字
+const addKeyWord = (theme, keyword) => new Promise( (resolve, reject) => {
+    MongoClient.connect(url, (err, client) => {
+        if (err) throw err;
+        const col = client.db(dbName).collection('theme');
+        col.updateOne({'name': theme},{'$addToSet':{'keywords':keyword}},[true,true],(err, result) => {
+            if (err) reject(err);
+            resolve('insert key success', String(result));
+            client.close();
+        });
+    });
+});
+
+//给主题添加过滤字
+const addFilterWord = (theme, filterword) => new Promise( (resolve, reject) => {
+    MongoClient.connect(url, (err, client) => {
+        if (err) throw err;
+        const col = client.db(dbName).collection('theme');
+        col.updateOne({'name': theme},{'$addToSet':{'filterwords':filterword}},[true,true],(err, result) => {
+            if (err) reject(err);
+            resolve('insert filter success', String(result));
+            client.close();
+        });
+    });
+});
+
+//根据主题查询过滤字段
+const getFilterByTheme = theme => new Promise((resolve, reject) => {
+    MongoClient.connect(url, (err, client) => {
+        if (err) throw err;
+        const themeCol = client.db(dbName).collection('theme');
+        themeCol.findOne({'name': theme},function (err, result) {
+            if (err) reject(err);
+            if (result) {
+                const filterwords = result.filterwords;
+                if (filterwords) {
+                    client.close();
+                    resolve(filterwords);
+                } else {
+                    client.close();
+                    reject(null)
+                }
+            } else {
+                client.close();
+                reject(null)
+            }
+        });
+    });
+});
+
+// getFilterByTheme('数据分析').then( res => {
+//     console.log(res);
+// }).catch( err => console.log(err));
+
+//根据主题查询数据
+const getBytheme = theme => new Promise( (resolve, reject) => {
+    MongoClient.connect(url, (err, client) => {
+        if (err) throw err;
+        const themeCol = client.db(dbName).collection('theme');
+        themeCol.findOne({'name': theme},function (err, result) {
+            if (err) reject(err);
+            try{
+                const keywords = result.keywords;
+                if (keywords) {
+                    const reg = `(${keywords.join('|')})`;
+                    const col = client.db(dbName).collection('ty_gp');
+                    col.find({'name':{'$regex':reg}}).toArray( (err, items) => {
+                        if (err) reject(err);
+                        client.close();
+                        resolve(items);
+                    });
+                } else {
+                    client.close();
+                    reject('10001');
+                }
+            }catch(error) {
+                client.close();
+                reject(error);
+            }
+        });
+    });
+});
+
+// getBytheme('数据分析').then( res=>{
+//     console.log(res);
+// });
 
 
 module.exports = {
     getAll,
-    getFrom
+    getFrom,
+    addTheme,
+    addKeyWord,
+    addFilterWord,
+    getFilterByTheme,
+    getBytheme
 };
